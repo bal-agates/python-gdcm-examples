26afc29f5e47e55bf0090b8fce03221353ea943f
bal-agates <bal_agates@yahoo.com>
2025-01-14 13:47:56 -0600

    Added do_tests.sh.  All included tests seem to run.

---
+++ do_tests.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env zsh
+
+apps=("ConvertNumpy.py" "ConvertPIL.py" "ConvertMPL.py")
+test_files=`ls test_data/*.dcm`
+for app in $apps
+do
+    for test_file in ${=test_files}
+    do
+        echo python $app $test_file
+        python $app $test_file
+    done
+    echo
+done
