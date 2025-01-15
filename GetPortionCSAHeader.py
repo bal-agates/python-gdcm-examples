1a7617756c5366b7d302cceafd481f28d4a1f27b
bal-agates <bal_agates@yahoo.com>
2025-01-15 09:13:22 -0600

    ruff format GetPortionCSAHeader.py

--- GetPortionCSAHeader.py
+++ GetPortionCSAHeader.py
@@ -26,43 +26,44 @@ import sys
 import gdcm
 
 if __name__ == "__main__":
+    file = sys.argv[1]
 
-  file = sys.argv[1]
+    r = gdcm.Reader()
+    r.SetFileName(file)
+    if not r.Read():
+        sys.exit(1)
 
-  r = gdcm.Reader()
-  r.SetFileName( file )
-  if not r.Read():
-    sys.exit(1)
+    ds = r.GetFile().GetDataSet()
+    csa_t1 = gdcm.CSAHeader()
+    csa_t2 = gdcm.CSAHeader()
+    # print csa
+    t1 = csa_t1.GetCSAImageHeaderInfoTag()
+    print(t1)
+    t2 = csa_t2.GetCSASeriesHeaderInfoTag()
+    print(t2)
+    # Let's do it for t1:
+    if ds.FindDataElement(t1):
+        csa_t1.LoadFromDataElement(ds.GetDataElement(t1))
+        print(csa_t1)
 
-  ds = r.GetFile().GetDataSet()
-  csa_t1 = gdcm.CSAHeader()
-  csa_t2 = gdcm.CSAHeader()
-  #print csa
-  t1 = csa_t1.GetCSAImageHeaderInfoTag()
-  print(t1)
-  t2 = csa_t2.GetCSASeriesHeaderInfoTag()
-  print(t2)
-  # Let's do it for t1:
-  if ds.FindDataElement( t1 ):
-    csa_t1.LoadFromDataElement( ds.GetDataElement( t1 ) )
-    print(csa_t1)
+    # Now let's pretend we are only interested in B_value and DiffusionGradientDirection entries:
+    bvalues = csa_t1.GetCSAElementByName("B_value")  # WARNING: it is case sensitive !
+    print(bvalues)
 
-  # Now let's pretend we are only interested in B_value and DiffusionGradientDirection entries:
-  bvalues = csa_t1.GetCSAElementByName( "B_value" ) # WARNING: it is case sensitive !
-  print(bvalues)
+    diffgraddir = csa_t1.GetCSAElementByName(
+        "DiffusionGradientDirection"
+    )  # WARNING: it is case sensitive !
+    print(diffgraddir)
 
-  diffgraddir = csa_t1.GetCSAElementByName( "DiffusionGradientDirection" ) # WARNING: it is case sensitive !
-  print(diffgraddir)
+    # repeat for t2 if you like it:
+    if ds.FindDataElement(t2):
+        csa_t2.LoadFromDataElement(ds.GetDataElement(t2))
+        # print(csa_t2)
 
-  # repeat for t2 if you like it:
-  if ds.FindDataElement( t2 ):
-    csa_t2.LoadFromDataElement( ds.GetDataElement( t2 ) )
-    # print(csa_t2)
+    gdt = csa_t2.GetCSAElementByName("GradientDelayTime")
+    print(gdt)
 
-  gdt = csa_t2.GetCSAElementByName( "GradientDelayTime" )
-  print(gdt)
-
-  bv = gdt.GetByteValue()
-  #print bv
-  str = bv.GetPointer()
-  print(str.split("\\"))
+    bv = gdt.GetByteValue()
+    # print bv
+    str = bv.GetPointer()
+    print(str.split("\\"))
