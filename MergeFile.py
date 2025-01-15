5153b60e120d6478dc0ab901e3c69c67a871ac23
bal-agates <bal_agates@yahoo.com>
2025-01-14 18:59:15 -0600

    ruff format MergeFile.py.

--- MergeFile.py
+++ MergeFile.py
@@ -27,32 +27,31 @@ import sys
 import gdcm
 
 if __name__ == "__main__":
+    file1 = sys.argv[1]
+    file2 = sys.argv[2]
 
-  file1 = sys.argv[1]
-  file2 = sys.argv[2]
+    r1 = gdcm.ImageReader()
+    r1.SetFileName(file1)
+    if not r1.Read():
+        sys.exit(1)
 
-  r1 = gdcm.ImageReader()
-  r1.SetFileName( file1 )
-  if not r1.Read():
-    sys.exit(1)
+    r2 = gdcm.ImageReader()
+    r2.SetFileName(file2)
+    if not r2.Read():
+        sys.exit(1)
 
-  r2 = gdcm.ImageReader()
-  r2.SetFileName( file2 )
-  if not r2.Read():
-    sys.exit(1)
+    # Image from r2 could be Secondary Capture and thus would not contains neither IPP nor IOP
+    # Instead always prefer to only copy the Raw Data Element.
+    # Warning ! Image need to be identical ! Only the value of Stored Pixel can be different.
+    r1.GetImage().SetDataElement(r2.GetImage().GetDataElement())
 
-  # Image from r2 could be Secondary Capture and thus would not contains neither IPP nor IOP
-  # Instead always prefer to only copy the Raw Data Element.
-  # Warning ! Image need to be identical ! Only the value of Stored Pixel can be different.
-  r1.GetImage().SetDataElement( r2.GetImage().GetDataElement() )
+    w = gdcm.ImageWriter()
+    w.SetFile(r1.GetFile())
+    # w.SetImage( r2.GetImage() )  # See comment above
+    w.SetImage(r1.GetImage())
 
-  w = gdcm.ImageWriter()
-  w.SetFile( r1.GetFile() )
-  #w.SetImage( r2.GetImage() )  # See comment above
-  w.SetImage( r1.GetImage() )
+    w.SetFileName("merge.dcm")
+    if not w.Write():
+        sys.exit(1)
 
-  w.SetFileName( "merge.dcm" )
-  if not w.Write():
-    sys.exit(1)
-
-  sys.exit(0)
+    sys.exit(0)
