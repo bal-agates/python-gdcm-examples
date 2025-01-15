a6b1049c2772aa016aa10fbc972d278133a39136
bal-agates <bal_agates@yahoo.com>
2025-01-15 11:17:07 -0600

    Updated do_tests.sh with something to run all source.  Currently manually check outputs.

--- do_tests.sh
+++ do_tests.sh
@@ -12,3 +12,59 @@ do
     done
     echo
 done
+
+# Should capture stdout.
+
+# Tested above on multiple files
+# python HelloWorld.py test_data/examples_rgb_color.dcm
+# python FindAllPatientName.py test_data/examples_rgb_color.dcm
+
+python MergeFile.py test_data/MR_small_padded.dcm test_data/examples_rgb_color.dcm
+gdcmdiff test_data/MR_small_padded.dcm merge.dcm
+
+python ScanDirectory.py test_data
+
+python RemovePrivateTags.py test_data/examples_ybr_color.dcm rem_pvt.dcm
+gdcmdiff test_data/examples_ybr_color.dcm rem_pvt.dcm
+
+python DumbAnonymizer.py test_data/MR_small_padded.dcm anon.dcm
+gdcmdiff test_data/MR_small_padded.dcm anon.dcm
+
+python ExtractImageRegion.py test_data/012345.002.050.dcm
+md5 frame.raw
+
+python FixCommaBug.py test_data/012345.002.050.dcm fix.dcm
+gdcmdiff test_data/012345.002.050.dcm fix.dcm
+
+python PrivateDict.py
+
+python ReadAndDumpDICOMDIR.py test_data/DICOMDIR
+
+python SortImage.py test_data
+
+python WriteBuffer.py test_data/examples_rgb_color.dcm wbuf
+
+python AddPrivateAttribute.py test_data/examples_rgb_color.dcm addattr.dcm
+
+python DecompressImage.py test_data/examples_ybr_color.dcm j.dcm
+
+python ManipulateSequence.py test_data/examples_ybr_color.dcm manip_sq.dcm
+gdcmdiff test_data/examples_ybr_color.dcm manip_sq.dcm
+
+python NewSequence.py test_data/examples_ybr_color.dcm new_sq.dcm
+gdcmdiff test_data/examples_ybr_color.dcm new_sq.dcm
+
+python ManipulateFile.py test_data/examples_ybr_color.dcm manip_file.dcm
+gdcmdiff test_data/examples_ybr_color.dcm manip_file.dcm
+
+python GetPortionCSAHeader.py test_data/examples_rgb_color.dcm
+
+python ManipulateFile.py test_data/examples_ybr_color.dcm manip_file.dcm
+gdcmdiff test_data/examples_ybr_color.dcm manip_file.dcm
+
+python PhilipsPrivateRescaleInterceptSlope.py test_data/examples_ybr_color.dcm
+gdcmdiff test_data/examples_ybr_color.dcm philips_rescaled.dcm
+
+python PlaySound.py test_data/examples_ybr_color.dcm
+
+python ReWriteSCAsMR.py test_data/MR_small_padded.dcm rewrite.dcm
