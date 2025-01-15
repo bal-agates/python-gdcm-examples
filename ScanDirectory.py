2f14cf861e6e11709e7841abcf19ef2b96bcbebe
bal-agates <bal_agates@yahoo.com>
2025-01-14 19:18:31 -0600

    ScanDirectory.py converted to Python 3.

--- ScanDirectory.py
+++ ScanDirectory.py
@@ -18,21 +18,21 @@ import sys,os
 class ProgressWatcher(gdcm.SimpleSubjectWatcher):
   def ShowProgress(self, sender, event):
     pe = gdcm.ProgressEvent.Cast(event)
-    print pe.GetProgress()
+    print(pe.GetProgress())
   def EndFilter(self):
-    print "Yay ! I am done"
+    print("Yay ! I am done")
 
 if __name__ == "__main__":
   directory = sys.argv[1]
 
   # Define the set of tags we are interested in
-  t1 = gdcm.Tag(0x8,0x8);
-  t2 = gdcm.Tag(0x10,0x10);
+  t1 = gdcm.Tag(0x8,0x8)
+  t2 = gdcm.Tag(0x10,0x10)
 
   # Iterate over directory
-  d = gdcm.Directory();
-  nfiles = d.Load( directory );
-  if(nfiles == 0): sys.exit(1);
+  d = gdcm.Directory()
+  nfiles = d.Load( directory )
+  if(nfiles == 0): sys.exit(1)
   # System.Console.WriteLine( "Files:\n" + d.toString() );
 
   filenames = d.GetFilenames()
@@ -41,16 +41,16 @@ if __name__ == "__main__":
   gdcm.Trace.WarningOff()
 
   # instantiate Scanner:
-  sp = gdcm.Scanner.New();
+  sp = gdcm.Scanner.New()
   s = sp.__ref__()
   w = ProgressWatcher(s, 'Watcher')
 
-  s.AddTag( t1 );
-  s.AddTag( t2 );
-  b = s.Scan( filenames );
-  if(not b): sys.exit(1);
+  s.AddTag( t1 )
+  s.AddTag( t2 )
+  b = s.Scan( filenames )
+  if(not b): sys.exit(1)
 
-  print "success" ;
+  print("success")
   #print s
 
   pttv = gdcm.PythonTagToValue( s.GetMapping( filenames[1] ) )
@@ -62,7 +62,7 @@ if __name__ == "__main__":
     # Warning value can be None
     tag = pttv.GetCurrentTag()
     value = pttv.GetCurrentValue()
-    print tag,"->",value
+    print(tag,"->",value)
     # increment iterator
     pttv.Next()
 
