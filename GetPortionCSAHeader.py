13fbccb235f83c01fe201d78c52f8389a1567160
bal-agates <bal_agates@yahoo.com>
2025-01-15 09:12:44 -0600

    Converted prints to Python 3.

--- GetPortionCSAHeader.py
+++ GetPortionCSAHeader.py
@@ -39,30 +39,30 @@ if __name__ == "__main__":
   csa_t2 = gdcm.CSAHeader()
   #print csa
   t1 = csa_t1.GetCSAImageHeaderInfoTag()
-  print t1
+  print(t1)
   t2 = csa_t2.GetCSASeriesHeaderInfoTag()
-  print t2
+  print(t2)
   # Let's do it for t1:
   if ds.FindDataElement( t1 ):
     csa_t1.LoadFromDataElement( ds.GetDataElement( t1 ) )
-    print csa_t1
+    print(csa_t1)
 
   # Now let's pretend we are only interested in B_value and DiffusionGradientDirection entries:
   bvalues = csa_t1.GetCSAElementByName( "B_value" ) # WARNING: it is case sensitive !
-  print bvalues
+  print(bvalues)
 
   diffgraddir = csa_t1.GetCSAElementByName( "DiffusionGradientDirection" ) # WARNING: it is case sensitive !
-  print diffgraddir
+  print(diffgraddir)
 
   # repeat for t2 if you like it:
   if ds.FindDataElement( t2 ):
     csa_t2.LoadFromDataElement( ds.GetDataElement( t2 ) )
-    # print csa_t2
+    # print(csa_t2)
 
   gdt = csa_t2.GetCSAElementByName( "GradientDelayTime" )
-  print gdt
+  print(gdt)
 
   bv = gdt.GetByteValue()
   #print bv
   str = bv.GetPointer()
-  print str.split("\\")
+  print(str.split("\\"))
