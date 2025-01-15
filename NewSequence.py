0e962c86a562b2ca5b3ef2aac9b53696aecc08e9
bal-agates <bal_agates@yahoo.com>
2025-01-15 09:07:06 -0600

    ruff format NewSequence.py

--- NewSequence.py
+++ NewSequence.py
@@ -25,46 +25,45 @@ import sys
 import gdcm
 
 if __name__ == "__main__":
+    file1 = sys.argv[1]
+    file2 = sys.argv[2]
 
-  file1 = sys.argv[1]
-  file2 = sys.argv[2]
+    r = gdcm.Reader()
+    r.SetFileName(file1)
+    if not r.Read():
+        sys.exit(1)
 
-  r = gdcm.Reader()
-  r.SetFileName( file1 )
-  if not r.Read():
-    sys.exit(1)
+    f = r.GetFile()
+    ds = f.GetDataSet()
+    # tsis = gdcm.Tag(0x0008,0x2112) # SourceImageSequence
 
-  f = r.GetFile()
-  ds = f.GetDataSet()
-  #tsis = gdcm.Tag(0x0008,0x2112) # SourceImageSequence
+    # Create a dataelement
+    de = gdcm.DataElement(gdcm.Tag(0x0010, 0x2180))
+    de.SetByteStringValue("Occupation")
+    de.SetVR(gdcm.VR(gdcm.VR.SH))
 
-  # Create a dataelement
-  de = gdcm.DataElement(gdcm.Tag(0x0010, 0x2180))
-  de.SetByteStringValue("Occupation")
-  de.SetVR(gdcm.VR(gdcm.VR.SH))
+    # Create an item
+    it = gdcm.Item()
+    it.SetVLToUndefined()  # Needed to not popup error message
+    # it.InsertDataElement(de)
+    nds = it.GetNestedDataSet()
+    nds.Insert(de)
 
-  # Create an item
-  it=gdcm.Item()
-  it.SetVLToUndefined()      # Needed to not popup error message
-  #it.InsertDataElement(de)
-  nds=it.GetNestedDataSet()
-  nds.Insert(de)
+    # Create a Sequence
+    sq = gdcm.SequenceOfItems().New()
+    sq.SetLengthToUndefined()
+    sq.AddItem(it)
 
-  # Create a Sequence
-  sq=gdcm.SequenceOfItems().New()
-  sq.SetLengthToUndefined()
-  sq.AddItem(it)
+    # Insert sequence into data set
+    des = gdcm.DataElement(gdcm.Tag(0x0400, 0x0550))
+    des.SetVR(gdcm.VR(gdcm.VR.SQ))
+    des.SetValue(sq.__ref__())
+    des.SetVLToUndefined()
 
-  # Insert sequence into data set
-  des=gdcm.DataElement(gdcm.Tag(0x0400,0x0550))
-  des.SetVR(gdcm.VR(gdcm.VR.SQ))
-  des.SetValue(sq.__ref__())
-  des.SetVLToUndefined()
+    ds.Insert(des)
 
-  ds.Insert(des)
-
-  w = gdcm.Writer()
-  w.SetFile( f )
-  w.SetFileName( file2 )
-  if not w.Write():
-    sys.exit(1)
+    w = gdcm.Writer()
+    w.SetFile(f)
+    w.SetFileName(file2)
+    if not w.Write():
+        sys.exit(1)
