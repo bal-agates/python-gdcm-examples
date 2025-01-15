85feaab7cd6bc22bd3d0ee16f4068d3cfcfb4bdf
bal-agates <bal_agates@yahoo.com>
2025-01-14 21:30:22 -0600

    Modified echo to make it easier to see command.

--- do_tests.sh
+++ do_tests.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env zsh
 
-apps=("ConvertNumpy.py" "ConvertPIL.py" "ConvertMPL.py")
+apps=("ConvertNumpy.py" "ConvertPIL.py" "ConvertMPL.py" "HelloWorld.py" "FindAllPatientName.py")
+# apps=("HelloWorld.py")
 test_files=`ls test_data/*.dcm`
 for app in $apps
 do
     for test_file in ${=test_files}
     do
-        echo python $app $test_file
+        echo "$ python $app $test_file"
         python $app $test_file
     done
     echo
