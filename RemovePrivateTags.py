8ddb08ed29e8b9beaee47a43b187763096f6951f
bal-agates <bal_agates@yahoo.com>
2025-01-14 19:38:31 -0600

    ruff format RemovePrivateTags.py.

--- RemovePrivateTags.py
+++ RemovePrivateTags.py
@@ -23,29 +23,28 @@ import gdcm
 
 
 if __name__ == "__main__":
+    file1 = sys.argv[1]
+    file2 = sys.argv[2]
 
-  file1 = sys.argv[1]
-  file2 = sys.argv[2]
+    # Instantiate the reader.
+    r = gdcm.Reader()
+    r.SetFileName(file1)
+    if not r.Read():
+        sys.exit(1)
 
-  # Instantiate the reader.
-  r = gdcm.Reader()
-  r.SetFileName( file1 )
-  if not r.Read():
-    sys.exit(1)
+    # Remove private tags
+    ano = gdcm.Anonymizer()
+    ano.SetFile(r.GetFile())
+    if not ano.RemovePrivateTags():
+        sys.exit(1)
 
-  # Remove private tags
-  ano = gdcm.Anonymizer()
-  ano.SetFile( r.GetFile() )
-  if not ano.RemovePrivateTags():
-    sys.exit(1)
+    # Write DICOM file
+    w = gdcm.Writer()
+    w.SetFile(ano.GetFile())
+    # w.CheckFileMetaInformationOff() # Do not attempt to check meta header
+    w.SetFileName(file2)
+    if not w.Write():
+        sys.exit(1)
 
-  # Write DICOM file
-  w = gdcm.Writer()
-  w.SetFile( ano.GetFile() )
-  #w.CheckFileMetaInformationOff() # Do not attempt to check meta header
-  w.SetFileName( file2 )
-  if not w.Write():
-    sys.exit(1)
-
-  # It is usually a good idea to exit the script with an error, as gdcm does not remove partial (incorrect) DICOM file
-  # (application level)
+    # It is usually a good idea to exit the script with an error, as gdcm does not remove partial (incorrect) DICOM file
+    # (application level)
