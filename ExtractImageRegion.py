4509b4cd29eda955921b3d9e73b66b0e2de7dceb
bal-agates <bal_agates@yahoo.com>
2025-01-14 20:30:25 -0600

    ruff format ExtractImageRegion.py.

--- ExtractImageRegion.py
+++ ExtractImageRegion.py
@@ -14,75 +14,76 @@
 
 """
 
- This small code shows how to use the gdcm.ImageRegionReader API
- In this example we are taking each frame by frame and dump them to
- /tmp/frame.raw.
+This small code shows how to use the gdcm.ImageRegionReader API
+In this example we are taking each frame by frame and dump them to
+/tmp/frame.raw.
 
- Usage:
- $ ExtractImageRegion.py input.dcm
+Usage:
+$ ExtractImageRegion.py input.dcm
 
- Example:
- $ ExtractImageRegion.py gdcmData/012345.002.050.dcm
- $ md5sum /tmp/frame.raw
- d594a5e2fde12f32b6633ca859b4d4a6  /tmp/frame.raw
- $ gdcminfo --md5sum gdcmData/012345.002.050.dcm
- [...]
- md5sum: d594a5e2fde12f32b6633ca859b4d4a6
+Example:
+$ ExtractImageRegion.py gdcmData/012345.002.050.dcm
+$ md5sum /tmp/frame.raw
+d594a5e2fde12f32b6633ca859b4d4a6  /tmp/frame.raw
+$ gdcminfo --md5sum gdcmData/012345.002.050.dcm
+[...]
+md5sum: d594a5e2fde12f32b6633ca859b4d4a6
 """
 
 import gdcm
 
 if __name__ == "__main__":
-  import sys
-  filename = sys.argv[1]
+    import sys
 
-  file_size = gdcm.System.FileSize(filename)
+    filename = sys.argv[1]
 
-  # instantiate the reader:
-  reader = gdcm.ImageRegionReader()
-  reader.SetFileName( filename )
+    file_size = gdcm.System.FileSize(filename)
 
-  # pull DICOM info:
-  if not reader.ReadInformation():
-    sys.exit(1)
+    # instantiate the reader:
+    reader = gdcm.ImageRegionReader()
+    reader.SetFileName(filename)
 
-  # store current offset:
-  cur_pos = reader.GetStreamCurrentPosition()
+    # pull DICOM info:
+    if not reader.ReadInformation():
+        sys.exit(1)
 
-  remaining = file_size - cur_pos
+    # store current offset:
+    cur_pos = reader.GetStreamCurrentPosition()
 
-  print(f"Remaining bytes to read (Pixel Data): {remaining}")
+    remaining = file_size - cur_pos
 
-  # Get file infos
-  f = reader.GetFile()
+    print(f"Remaining bytes to read (Pixel Data): {remaining}")
 
-  # get some info about image
-  dims = gdcm.ImageHelper.GetDimensionsValue(f)
-  print(dims)
-  pf = gdcm.ImageHelper.GetPixelFormatValue (f)
-  pixelsize = pf.GetPixelSize()
-  pi = gdcm.ImageHelper.GetPhotometricInterpretationValue(f)
-  print( pi )
+    # Get file infos
+    f = reader.GetFile()
 
-  # buffer to get the pixels
-  buffer = bytearray( dims[0] * dims[1] * pixelsize )
+    # get some info about image
+    dims = gdcm.ImageHelper.GetDimensionsValue(f)
+    print(dims)
+    pf = gdcm.ImageHelper.GetPixelFormatValue(f)
+    pixelsize = pf.GetPixelSize()
+    pi = gdcm.ImageHelper.GetPhotometricInterpretationValue(f)
+    print(pi)
 
-  # define a simple box region.
-  box = gdcm.BoxRegion()
-  for z in range(0, dims[2]):
-    # Define that I want the image 0, full size (dimx x dimy pixels)
-    # and do that for each z:
-    box.SetDomain(0, dims[0] - 1, 0, dims[1] - 1, z, z)
-    #print( box.toString() )
-    reader.SetRegion( box )
+    # buffer to get the pixels
+    buffer = bytearray(dims[0] * dims[1] * pixelsize)
 
-    # reader will try to load the uncompressed image region into buffer.
-    # the call returns an error when buffer.Length is too small. For instance
-    # one can call:
-    # uint buf_len = reader.ComputeBufferLength() // take into account pixel size
-    # to get the exact size of minimum buffer
-    if reader.ReadIntoBuffer(buffer):
-      open('frame.raw', 'wb').write(buffer)
-    else:
-      #throw new Exception("can't read pixels error")
-      sys.exit(1)
+    # define a simple box region.
+    box = gdcm.BoxRegion()
+    for z in range(0, dims[2]):
+        # Define that I want the image 0, full size (dimx x dimy pixels)
+        # and do that for each z:
+        box.SetDomain(0, dims[0] - 1, 0, dims[1] - 1, z, z)
+        # print( box.toString() )
+        reader.SetRegion(box)
+
+        # reader will try to load the uncompressed image region into buffer.
+        # the call returns an error when buffer.Length is too small. For instance
+        # one can call:
+        # uint buf_len = reader.ComputeBufferLength() // take into account pixel size
+        # to get the exact size of minimum buffer
+        if reader.ReadIntoBuffer(buffer):
+            open("frame.raw", "wb").write(buffer)
+        else:
+            # throw new Exception("can't read pixels error")
+            sys.exit(1)
